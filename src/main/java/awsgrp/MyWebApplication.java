0d5fb445496aed1ddc0b5cdd2964edaa4523016e
krish <sagarcloud87@gmail.com>
2021-07-01 07:26:45 +0530

    src is

---
+++ src/main/java/awsgrp/MyWebApplication.java
@@ -0,0 +1,16 @@
+package awsgrp;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.PerspectivesModule;
+
+public class MyWebApplication extends OrienteerWebApplication
+{
+	@Override
+	public void init()
+	{
+		super.init();
+		mountPackage("awsgrp.web");
+		registerModule(DataModel.class);
+	}
+	
+}
